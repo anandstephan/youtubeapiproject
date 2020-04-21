8ff6f00c4a2766bacfabc70e76b82bd34dd298db
anandcool <anand9412868527@gmail.com>
2020-04-21 21:34:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Grid} from '@material-ui/core';
+import youtube from './api/youtube';
+import VideoList from './components/VideoList';
+import {SearchBar,VideoDetails} from './components'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+    state={
+        videos:[],
+        selectedVideo:null
+    }
+    componentDidMount(){
+        this.handleSubmit('Online Quiz System project in php -part#1');
+    }
+    onVideoSelect = (video)=>{
+        this.setState({selectedVideo:video});
+    }
+    handleSubmit = async (searchTerm)=>{
+     const response = await youtube.get('search',{
+        params:{
+            part:'snippet',
+            maxResults:5,
+            key:'AIzaSyCemDMBGDcFqAHn0syDEMDS8zeC59wzF0o',
+            q:searchTerm
+        }
+     });
+     this.setState({
+         videos:response.data.items,
+         selectedVideo:response.data.items[0]
+     })
+    }
+    render(){
+        const {selectedVideo,videos} = this.state;
+        return(
+            <Grid justify="center" container spacing={10}>
+                <Grid item xs={12}>
+                    <SearchBar onFormSubmit={this.handleSubmit}/>
+                </Grid>
+                <Grid item xs={8}>
+                <VideoDetails video={selectedVideo}/>
+                </Grid>
+                <Grid item xs={4}>
+                <VideoList videos = {videos} onVideoSelect={this.onVideoSelect}/>
+                </Grid>
+            </Grid>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Paper, TextField} from '@material-ui/core';
+
+class SearchBar extends React.Component{
+    state= {
+        searchTerm:''
+        }
+    
+    handleChange = (event) => this.setState({searchTerm:event.target.value});
+    handleSubmit = (event)=>{
+        event.preventDefault();
+        // console.log("i")
+        const {searchTerm} = this.state;
+        const {onFormSubmit} = this.props;
+        onFormSubmit(searchTerm)
+    }
+    render() {
+        return (
+            <Paper elevation={6} style={{padding:'25px'}}>
+                <form onSubmit={this.handleSubmit}>
+                    <TextField fullWidth label="Search ..." onChange={this.handleChange}></TextField>
+                </form>
+            </Paper>
+        );
+    }
+}
+export default SearchBar;
